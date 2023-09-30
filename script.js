12109959dec9e9166ac0a140be1cb762ce3df2a7
Franz Knipp <franz@qnipp.com>
2023-09-30 12:20:46 +0000

    Add counter

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const katze = document.querySelector(".katze");
 const herz = document.querySelector(".herz");
 
+let anzahl = 0;
+
+function aktualisiereAnzahl() {
+    document.querySelector(".zaehler").textContent = anzahl;
+}
+aktualisiereAnzahl();
+
 function zeigeHerz() {
+    console.log("zeigeHerz");
     herz.style.display = "block";
+    anzahl++;
+    aktualisiereAnzahl();
 }
 
 function verschwindeHerz(e) {
+    console.log("verschwindeHerz");
     e.stopPropagation();
     herz.style.display = "none";
 }
